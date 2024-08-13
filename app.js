920f02968cf0300b6b75e001f0b8fbf2a6d0daaf
greatxrider <daligdig.jephmari@gmail.com>
2024-08-13 15:34:40 +0800

    update app

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const data = require('./data/pfs.json');
+const cors = require('cors');
 
-const app = express();
+const app = express();  // Initialize Express app
+
+// Use CORS middleware
+app.use(cors());
+
 const PORT = 8000;
 
+// Define route to serve JSON data
 app.get('/', (req, res) => {
     res.json(data);
 });
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`✅ Server is running on port ${PORT}`);
 });
